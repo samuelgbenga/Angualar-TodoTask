4d4fd6fa7abfa90f29a45bc374ddaf2b26aa599f
samuelgbenga <samuelgbenga972@gmail.com>
2024-12-06 03:35:24 +0100

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Guid } from "guid-typescript";
+import { Todo } from "../../src/models/todo.model";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  todos: Todo[] = []
+
+  onSubmit(form: NgForm){
+    let todo = new Todo(Guid.create(), form.value.title, false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+
+  onComplete(id: Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    let index = this.todos.indexOf(todo,0);
+    if(index > -1){
+      this.todos.splice(index,1);
+    }
+  }
 }
